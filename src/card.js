b0e6a25b499bae6f837d5a316e05e24caf8bfdad
yashs9131 <yashs9131@gmail.com>
2022-06-05 16:53:40 +0530

    fourth commit

--- src/card.js
+++ src/card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 
-const Card = () => {
+const Card = (props) => {
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow'>
-            <img alt='robots' src='https://robohash.org/test?size=200x200' />
+            <img alt='robots' src={`https://robohash.org/${props.id}?size=200x200`} />
             <div>
-                <h2>Jane Doe</h2>
-                <p>Jane.doe@gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     )
